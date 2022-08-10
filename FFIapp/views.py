06cd1cd6ba6ebdfb63001fcd98237cf4cee5e4c6
TheJonry <110304328+TheJonry@users.noreply.github.com>
2022-08-10 05:17:04 -0400

    mobile fix

--- FFIapp/views.py
+++ FFIapp/views.py
@@ -1,12 +1,12 @@
+import re
 from django.shortcuts import render, redirect
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', )
 
 
 def products(request):
