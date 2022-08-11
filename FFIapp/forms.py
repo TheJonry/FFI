11b2d8dbe2b8a0a1dbef9c9578da8120efcd3d2b
TheJonry <110304328+TheJonry@users.noreply.github.com>
2022-08-11 03:31:29 -0400

    a

--- FFIapp/forms.py
+++ FFIapp/forms.py
@@ -1,4 +1,11 @@
 from socket import fromshare
 from django import forms
+from django.forms import ModelForm
+from FFIapp.models import models, newsPost
 
-   
+class newsPostForm(ModelForm):
+    class Meta:
+        model = newsPost
+        fields = ['title', 'message']
+
+form = newsPostForm()
--- FFIapp/models.py
+++ FFIapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django import forms
 
 class UserProfile(AbstractUser):
     age = models.PositiveIntegerField(name="age", null="yes", blank="yes")
@@ -9,4 +10,7 @@ class newsPost(models.Model):
     author = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name="posts", null="True")
     created_at = models.DateTimeField(auto_now_add="True")
     title = models.CharField(max_length=100)
-    message = models.CharField(max_length=1000)
+    message = models.TextField(max_length=5000)
+
+    class Meta:
+        ordering = ('-created_at',)
