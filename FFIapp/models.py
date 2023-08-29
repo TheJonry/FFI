3d91b9829d113538455c72bf93b289f233850a52
TheJonry <110304328+TheJonry@users.noreply.github.com>
2023-08-29 02:47:52 -0400

    DB updates

--- FFIapp/models.py
+++ FFIapp/models.py
@@ -18,20 +18,47 @@ class newsPost(models.Model):
     def __str__(self):
         return self.title + " | " + self.author
 
-class boats(models.Model):
+class Services(models.Model):
     id = models.BigAutoField(primary_key=True)
-    
-class customers(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    Title = models.CharField(max_length=50)
+    Description = models.TextField(max_length=3000)
+    Unit_Price = models.PositiveSmallIntegerField(max_length=8)
+    Unit_Price_Description = models.CharField(max_length=50)
 
-class employees(models.Model):
+class Marinas(models.Model):
     id = models.BigAutoField(primary_key=True)
+    Street = models.CharField(max_length=50)
+    City = models.CharField(max_length=25)
+    Zip = models.PositiveSmallIntegerField(max_length=5)
 
-class services(models.Model):
+class Customers(models.Model):
     id = models.BigAutoField(primary_key=True)
+    First_Name = models.CharField(max_length=30)
+    Last_Name = models.CharField(max_length=30)
 
-class appointments(models.Model):
+class Boats(models.Model):
     id = models.BigAutoField(primary_key=True)
+    Owner = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    Size = models.PositiveSmallIntegerField(max_length=4)
+    Preferred_Interval = models.PositiveSmallIntegerField(max_length=3)
 
-class marinas(models.Model):
+class Employees(models.Model):
     id = models.BigAutoField(primary_key=True)
+    First_Name = models.CharField(max_length=30)
+    Last_Name = models.CharField(max_length=30)
+    Can_Do = models.ManyToManyField(Services)
+
+class Appointments(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    Date_Submitted = models.DateField(auto_now_add=True)
+    Date_Of_Appointment = models.DateField(auto_now=False, auto_now_add=False)
+    Boat = models.ForeignKey(Boats, on_delete=models.CASCADE)
+    Marina = models.ForeignKey(Marinas, on_delete=models.CASCADE)
+    Slip = models.CharField(max_length=15)
+    Employee = models.ForeignKey(Employees, on_delete=models.CASCADE)
+    Clean_Service = models.BooleanField(default=True)
+    Zinc_Service = models.BooleanField(default=False)
+    Zinc_Service_Units = models.PositiveSmallIntegerField(max_length=4)
+    Status = models.TextChoices("Status", "APPLIED CONFIRMED CANCELLED RESCHEDULED COMPLETE")
+
+
