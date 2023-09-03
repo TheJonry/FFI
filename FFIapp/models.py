83410650d1e8d0118788a6e610c6d56a693b88b0
TheJonry <110304328+TheJonry@users.noreply.github.com>
2023-09-03 00:04:05 -0400

    db update

--- FFIapp/models.py
+++ FFIapp/models.py
@@ -27,29 +27,42 @@ class Service(models.Model):
     def __str__(self):
         return self.Title
 
+class Region(models.Model):
+    Name = models.CharField(max_length=50)
+
 class Marina(models.Model):
     id = models.BigAutoField(primary_key=True)
     Name = models.CharField(max_length=50)
     Street = models.CharField(max_length=50)
     City = models.CharField(max_length=25)
     Zip = models.PositiveSmallIntegerField()
+    Region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True)
+    Note = models.TextField(max_length=500, default="No notes.")
 
 class Customer(models.Model):
     id = models.BigAutoField(primary_key=True)
-    First_Name = models.CharField(max_length=30)
-    Last_Name = models.CharField(max_length=30)
+    First_Name = models.CharField(max_length=30, null=True)
+    Last_Name = models.CharField(max_length=30, null=True)
+    Street = models.CharField(max_length=50, default="")
+    City = models.CharField(max_length=25, default="")
+    Zip = models.PositiveSmallIntegerField( null=True)
+    Note = models.TextField(max_length=500, default="No notes.")
+    Contact_Info = models.CharField(max_length=50, default="")
 
 class Boat(models.Model):
     id = models.BigAutoField(primary_key=True)
+    Name = models.CharField(max_length=50, default="Boat")
     Owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
     Size = models.PositiveSmallIntegerField()
     Preferred_Interval = models.PositiveSmallIntegerField()
+    Note = models.TextField(max_length=500, default="No notes.")
 
 class Employee(models.Model):
     id = models.BigAutoField(primary_key=True)
     First_Name = models.CharField(max_length=30)
     Last_Name = models.CharField(max_length=30)
-    Can_Do = models.ManyToManyField(Service) 
+    Can_Do = models.ManyToManyField(Service)
+    Note = models.TextField(max_length=500, default="No notes.") 
 
 class Appointment(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -63,5 +76,6 @@ class Appointment(models.Model):
     Zinc_Service = models.BooleanField(default=False)
     Zinc_Service_Units = models.PositiveSmallIntegerField()
     Status = models.TextChoices("Status", "APPLIED CONFIRMED CANCELLED RESCHEDULED COMPLETE")
+    Note = models.TextField(max_length=500, default="No notes.")
 
 
